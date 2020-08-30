c230152948cab9b9a50867b12c3e6db07517f113
Vaibhav Yadaram <yadaram.vaibhav@gmail.com>
2020-08-29 22:57:23 -0400

    Fetch list of searched movies

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,5 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Input = styled.input`
+  font-family: Arial;
+  font-weight: 700;
+  font-size: 20px;
+  margin: 0 0 20px 0;
+  text-align: center;
+`
+
+const Button = styled.button`
+  font-family: Arial;
+  font-weight: 700;
+  font-size: 20px;
+  margin: 0 0 20px 0;
+  text-align: center;
+  height: 50px;
+  width: 150px;
+  border: none;
+`
+
+const IndexPage = () => {
+  const url = "http://www.omdbapi.com/?i=tt3896198&apikey=e4f7e31a&s="
+  const [searchUrl, setUrl] = useState()
+
+  const clickHandler = () => {
+    const movieTitle = document.getElementById("movieTitle").value
+    setUrl(url + movieTitle)
+    getSearchResults().then(movies => console.log(movies))
+  }
+
+  const getSearchResults = () => {
+    const movieList = fetch(searchUrl)
+      .then(success => success.json())
+      .then(movies => {
+        return movies
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    return movieList
+  }
+
+  return (
+    <>
+      <Input type="text" id="movieTitle" placeholder="name" />
+      <Button onClick={clickHandler}>search</Button>
+      {/* {searchList} */}
+    </>
+  )
 }
+
+export default IndexPage
